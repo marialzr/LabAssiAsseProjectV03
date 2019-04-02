8671e0a5347d4ebc5dd55a4c9f94ba3bedf2c2b7
marialzr <maria_lazar97@yahoo.com>
2019-04-02 23:31:25 +0300

    add multiple test cases

--- src/test/java/ro/ubb/TestAddStudent.java
+++ src/test/java/ro/ubb/TestAddStudent.java
@@ -2,14 +2,14 @@ package ro.ubb;
 
 import junit.framework.TestCase;
 import org.junit.Test;
-import ro.ubb.src.Domain.Student;
 import ro.ubb.src.Exceptions.ValidatorException;
 import ro.ubb.src.Repository.XMLFileRepository.StudentXMLRepo;
 import ro.ubb.src.Service.XMLFileService.StudentXMLService;
 import ro.ubb.src.Validator.StudentValidator;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.List;
+import javax.xml.bind.ValidationException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestAddStudent extends TestCase {
     private StudentXMLService stsrv;
@@ -29,7 +29,27 @@ public class TestAddStudent extends TestCase {
     }
 
     @Test
-    public void testAdd() throws ValidatorException {
+    public void testAddStudent() throws ValidatorException {
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        stsrv.add(params);
+        int sizeAfter = sizeBefore + 1;
+        assertEquals(sizeAfter, stsrv.getSize());
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testAddStudentIdNull() {
+        id = null;
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        ValidatorException thrown = assertThrows(ValidatorException.class, () -> stsrv.add(params), "Expected doThing() to throw, but it didn't");
+
+        assertEquals(sizeBefore, stsrv.getSize());
+    }
+
+    @Test
+    public void testAddStudentIdNotNull() throws ValidatorException {
+        id = "1";
         int sizeBefore = stsrv.getSize();
         String params[] ={id, nume, grupa, email, prof};
         stsrv.add(params);
@@ -38,14 +58,82 @@ public class TestAddStudent extends TestCase {
     }
 
     @Test(expected = ValidatorException.class)
-    public void testWrongGrupa() {
-        grupa = "abc";
+    public void testAddStudentNumeNull() {
+        nume = "";
         int sizeBefore = stsrv.getSize();
         String params[] ={id, nume, grupa, email, prof};
-        ValidatorException thrown = assertThrows(ValidatorException.class,
-                () -> stsrv.add(params), "Expected doThing() to throw, but it didn't");
+        ValidatorException thrown = assertThrows(ValidatorException.class, () -> stsrv.add(params), "Expected doThing() to throw, but it didn't");
 
         assertEquals(sizeBefore, stsrv.getSize());
     }
 
+    @Test
+    public void testAddStudentNumeNotNull() throws ValidatorException{
+        nume = "Ema";
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        stsrv.add(params);
+        int sizeAfter = sizeBefore + 1;
+        assertEquals(sizeAfter, stsrv.getSize());
+    }
+
+    @Test
+    public void testAddStudentIntegerGrupa() {
+        grupa = "934";
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        ValidatorException thrown = assertThrows(ValidatorException.class, () -> stsrv.add(params), "Expected doThing() to throw, but it didn't");
+
+        assertEquals(sizeBefore, stsrv.getSize());
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void testAddStudentWrongGrupa() {
+        grupa = "ab";
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        ValidatorException thrown = assertThrows(ValidatorException.class, () -> stsrv.add(params), "Expected doThing() to throw, but it didn't");
+
+        assertEquals(sizeBefore, stsrv.getSize());
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void testAddStudenEmailNull() {
+        email = "";
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        ValidatorException thrown = assertThrows(ValidatorException.class, () -> stsrv.add(params), "Expected doThing() to throw, but it didn't");
+
+        assertEquals(sizeBefore, stsrv.getSize());
+    }
+
+    @Test
+    public void testAddStudentEmailNotNull() throws ValidatorException {
+        email = "ema.turcu2@gmail.com";
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        stsrv.add(params);
+        int sizeAfter = sizeBefore + 1;
+        assertEquals(sizeAfter, stsrv.getSize());
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void testAddStudentIndrumatorNull() {
+        prof = null;
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        ValidatorException thrown = assertThrows(ValidatorException.class, () -> stsrv.add(params), "Expected doThing() to throw, but it didn't");
+
+        assertEquals(sizeBefore, stsrv.getSize());
+    }
+
+    @Test
+    public void testAddStudentIndrumatorNotNull() throws ValidatorException {
+        prof = "Maria";
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        stsrv.add(params);
+        int sizeAfter = sizeBefore + 1;
+        assertEquals(sizeAfter, stsrv.getSize());
+    }
 }
