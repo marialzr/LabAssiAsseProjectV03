a8e467217885ebab7672f6f0322b81021854aa03
marialzr <maria_lazar97@yahoo.com>
2019-04-17 08:35:38 +0300

    Add integration tests

---
+++ src/test/java/ro/ubb/TestIntegration.java
@@ -0,0 +1,83 @@
+package ro.ubb;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import ro.ubb.src.Exceptions.ValidatorException;
+import ro.ubb.src.Repository.XMLFileRepository.NotaXMLRepo;
+import ro.ubb.src.Repository.XMLFileRepository.StudentXMLRepo;
+import ro.ubb.src.Repository.XMLFileRepository.TemaLabXMLRepo;
+import ro.ubb.src.Service.XMLFileService.NotaXMLService;
+import ro.ubb.src.Service.XMLFileService.StudentXMLService;
+import ro.ubb.src.Service.XMLFileService.TemaLabXMLService;
+import ro.ubb.src.Validator.NotaValidator;
+import ro.ubb.src.Validator.StudentValidator;
+import ro.ubb.src.Validator.TemaLabValidator;
+
+public class TestIntegration extends TestCase {
+    private NotaXMLService notaService;
+    private StudentXMLService stsrv;
+    private TemaLabXMLService temaService;
+
+    protected void setUp(){
+        StudentValidator vs=new StudentValidator();
+        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+        stsrv=new StudentXMLService(strepo);
+
+        NotaValidator vn=new NotaValidator();
+        NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
+        notaService=new NotaXMLService(ntrepo);
+
+        TemaLabValidator vt = new TemaLabValidator();
+        TemaLabXMLRepo temaLabXMLRepo = new TemaLabXMLRepo(vt, "temaXML.xml");
+        temaService = new TemaLabXMLService(temaLabXMLRepo);
+    }
+
+    @Test
+    public void testAddStudent() throws ValidatorException {
+        String id = "1";
+        String nume = "Name";
+        String grupa = "934";
+        String email = "name@gmail.com";
+        String prof = "Prof";
+
+        int sizeBefore = stsrv.getSize();
+        String params[] ={id, nume, grupa, email, prof};
+        stsrv.add(params);
+        int sizeAfter = sizeBefore + 1;
+        assertEquals(sizeAfter, stsrv.getSize());
+    }
+
+    public void testAddTemaLab() throws ValidatorException {
+        String id = "1";
+        String termenLimita = "6";
+        String saptamanaPredarii = "4";
+        String descriere = "description";
+
+        int sizeBefore = temaService.getSize();
+        String params[] = {id, descriere, termenLimita, saptamanaPredarii};
+        temaService.add(params);
+        int sizeAfter = sizeBefore + 1;
+        assertEquals(sizeAfter, temaService.getSize());
+    }
+
+    public void testAddAssignment() throws ValidatorException {
+        String id = "1";
+        String ids = "1";
+        String idt = "1";
+        String val = "3";
+        String data = "2018-03-01T13:15:12";
+
+        String params[] ={id,ids,idt,val,data};
+        int sizeBefore = notaService.getSize();
+        notaService.add(params);
+        int sizeAfter = sizeBefore + 1;
+        assertEquals(sizeAfter, notaService.getSize());
+    }
+
+    @Test
+    public void testMethods() throws ValidatorException {
+        testAddStudent();
+        testAddTemaLab();
+        testAddAssignment();
+    }
+}
